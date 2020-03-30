824836cbbef1cb3786d633b97fd1dda2fbeb8dd8
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-30 22:32:48 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist
+  attr_accessor :name, :artist#,:genres
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def songs
+     Song.all.select{|song|song.artist == self || song.genre == self }
+  end
+  def new_song(song,genres)
+
+    Song.new(song,self,genres)
+end
+def genres
+  songs.map{|song|song.genre}
+end
+end
---
+++ lib/genre.rb
@@ -0,0 +1,9 @@
+class Genre
+  attr_accessor :name, :songs
+  def initialize(name)
+    @name = name
+  end
+  # def songs
+  #
+  # end
+end
