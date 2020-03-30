b4c82b58d984c2039277503fe0edc7f1ccee60af
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-30 22:39:15 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,7 +1,12 @@
 class Genre
   attr_accessor :name, :songs
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
   end
   def songs
      Song.all.select{|song|song.artist == self || song.genre == self }
