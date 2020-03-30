bea6962f2b7a12566c8e567db194d2d57113d883
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-30 22:53:08 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,21 +1,14 @@
 
 class Appointment
-  attr_accessor :name
-  @@all = []
-  def initialize(name)
-    @name = name
+  attr_accessor :date, :patient, :doctor
+    @@all = []
+  def initialize(date,patient,doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
     @@all << self
   end
   def self.all
     @@all
   end
-#   def songs
-#      Song.all.select{|song|song.artist == self || song.genre == self }
-#   end
-#   def new_song(song,genres)
-#     Song.new(song,self,genres)
-# end
-# def genres
-#   songs.map{|song|song.genre}
-# end
 end
