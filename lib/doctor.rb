0bc6e0971e3a64beabaf30a0aee1265d248ffa03
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-30 23:18:06 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -9,7 +9,7 @@ class Doctor
     @@all
   end
    def appointments
-      Appointment.all.select{|appointment| appointment.doctor == self || appointment.patient == self}
+      Appointment.all.select{|appointment| appointment.doctor == self }
    end
    def new_appointment(patient,date)
       Appointment.new(date,patient,self)
