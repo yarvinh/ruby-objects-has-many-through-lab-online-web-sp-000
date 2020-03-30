f089f56907ffe79f65c4d6d942caa8f2d82c9977
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-30 23:41:31 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,17 @@
+class Genre
+  attr_accessor :name, :songs
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def songs
+     Song.all.select{|song|song.artist == self || song.genre == self }
+  end
+  def artists
+      songs.map{|song|song.artist}
+  end
+end
